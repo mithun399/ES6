50bb05e873418ab5beec9511e57dec2639e3ad22
mithun399 <msovik399@gmail.com>
2023-03-20 13:43:16 +0600

    arr.splice

---
+++ arraySplice.js
@@ -0,0 +1,18 @@
+var arr=[1,2,3,4,5,6]
+
+
+var res=arr.splice(1,2,7,8,9)
+
+console.log(res)
+
+console.log(arr)
+
+
+
+
+
+//splice mane new kichu include kora
+//splice main array k change kore
+//main array console log korle change hoa array show korbe 
+//r splice korata log korle show korbe j value she remove kore diyche
+//splice er main kaj ta hocche kono ekta array theke kichu value ber kore niye asha
