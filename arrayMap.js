7bba4ca07f236569d98da64b4d66218c985e7682
mithun399 <msovik399@gmail.com>
2023-03-21 15:36:38 +0600

    array map

---
+++ arrayMap.js
@@ -0,0 +1,10 @@
+    var arr = [1, 2, 3, 4]
+    let res = arr.map((num) => {
+        return 2 * num;
+    })
+    console.log(res)
+
+
+    //Map onekta for loop er moto kaj kore
+    //protita element er moddhe diye jai 
+    //main array k change korena
